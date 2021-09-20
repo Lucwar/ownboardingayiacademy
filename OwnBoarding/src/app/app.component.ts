40ce3434ede14fd6ffad1467f6780563497e84c9
LUCAS GUERRA <lucguerra99@hotmail.com>
2021-09-20 02:04:48 -0300

    Agrego seccion video y nuevas formas

--- OwnBoarding/src/app/app.component.ts
+++ OwnBoarding/src/app/app.component.ts
@@ -8,14 +8,17 @@ import { Attribute, Component } from '@angular/core';
 })
 export class AppComponent {
   
+  //Boton seleccionado por defecto
   selected = "button0";
 
   buttonSelect(aux: any){
+    //Saco la clase "selected" de todos los li
     for(let i = 0; i < 4; i++){
-      document.getElementsByClassName('aux')[i].classList.remove('selected');
+      document.getElementsByTagName('li')[i].classList.remove('selected');
     }
+    //Agrego la clase al seleccionado
     document.getElementsByClassName(aux)[0].classList.add('selected');
-    //Recorro el array de estados y los pongo todos en falso
+    //Asigno el valor al seleccionado
     this.selected = aux;
   }
 }
